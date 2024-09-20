ba12f840fa2405a91e5a963f89bc72f350b98808
asghar-0017 <asghar.usman0017@gmail.com>
2024-09-20 19:29:27 +0500

    .

--- src/services/clientServices.js
+++ src/services/clientServices.js
@@ -1,9 +1,6 @@
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 dotenv.config();
-// const dataInRepo = require('../Repository/clientRepo'); 
-
-
 
 const sendMessageTOService = async (bodyData) => {
     const transporter = nodemailer.createTransport({
@@ -19,7 +16,7 @@ const sendMessageTOService = async (bodyData) => {
         to: bodyData.email,
         subject: 'Thank you for your submission', 
         text: `Hi ${bodyData.name},\n\nThank you for your message. We have received the following information:\n\nName: ${bodyData.name}\nEmail: ${bodyData.email}\nMessage: ${bodyData.message}\n\nWe will get back to you soon.`,
-        html: `<p>Hi <b>${bodyData.namr}</b>,</p>
+        html: `<p>Hi <b>${bodyData.name}</b>,</p>
                <p>Thank you for your message. We have received the following information:</p>
                <ul>
                  <li><b>Name:</b> ${bodyData.name}</li>
@@ -30,9 +27,9 @@ const sendMessageTOService = async (bodyData) => {
     };
 
     const adminMailOptions = {
-        from: process.env.EMAIL, // Sender address
-        to: process.env.ADMIN_EMAIL, // Recipient address (admin)
-        subject: 'New Contact Form Submission', // Subject line
+        from: process.env.EMAIL,
+        to: process.env.ADMIN_EMAIL, 
+        subject: 'New Contact Form Submission',
         text: `You have a new contact form submission from ${bodyData.name}.\n\nMessage: ${bodyData.message}\n\nClient Email: ${bodyData.email}`,
         html: `<p>You have a new contact form submission from <b>${bodyData.name}</b>.</p>
                <p>Message: ${bodyData.message}</p>
@@ -41,19 +38,16 @@ const sendMessageTOService = async (bodyData) => {
 
     try {
         // Send both emails
-        // const savedContact = await dataInRepo(bodyData);
         const clientInfo = await transporter.sendMail(clientMailOptions);
         const adminInfo = await transporter.sendMail(adminMailOptions);
 
         return {
-            savedContact,
             clientMessageId: clientInfo.messageId,
             adminMessageId: adminInfo.messageId,
             previewUrl: nodemailer.getTestMessageUrl(adminInfo), 
         };
     } catch (error) {
         console.error('Error sending emails:', error);
-
     }
 };
 
