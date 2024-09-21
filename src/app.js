e698c4e90de185256be5e2924d3f461245915241
asghar-0017 <asghar.usman0017@gmail.com>
2024-09-21 17:48:31 +0500

    first commit

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
-const { ConnectDB } = require("./infrastructure/index.js");
+// const { ConnectDB } = require("./infrastructure/index.js");
 const Routes = require("./routes/clientRoutes.js");
 const app = express();
+const cors = require('cors');  // Corrected the package name
 const dotenv = require('dotenv');
 dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin:'*'
+}))
 
 Routes(app);
 
@@ -15,17 +19,20 @@ app.get("/", (req, res) => {
 })
 
 const start = () => {
-  ConnectDB()
-    .then(() => {
-      app.listen(process.env.PORT, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  // ConnectDB()
+  //   .then(() => {
+  //     app.listen(process.env.PORT, () => {
+  //       console.log(`Server is running on port ${process.env.PORT}`);
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  app.listen(process.env.PORT, () => {
+          console.log(`Server is running on port ${process.env.PORT}`);
+        });
+      }
 
-}
 
 
 module.exports = { start };
--- src/services/clientServices.js
+++ src/services/clientServices.js
@@ -13,41 +13,49 @@ const sendMessageTOService = async (bodyData) => {
 
     const clientMailOptions = {
         from: process.env.EMAIL, 
-        to: bodyData.email,
-        subject: 'Thank you for your submission', 
-        text: `Hi ${bodyData.name},\n\nThank you for your message. We have received the following information:\n\nName: ${bodyData.name}\nEmail: ${bodyData.email}\nMessage: ${bodyData.message}\n\nWe will get back to you soon.`,
+        to: bodyData.email, 
+        subject: 'Thank you for contacting us!', 
+        text: `Hi ${bodyData.name},\n\nThank you for your message. Here is the information you submitted:\n\nName: ${bodyData.name}\nEmail: ${bodyData.email}\nSubject: ${bodyData.subject}\nPhone: ${bodyData.phone}\nMessage: ${bodyData.message}\n\nWe will get back to you shortly.\n\nBest Regards,\nYour Company Name`,
         html: `<p>Hi <b>${bodyData.name}</b>,</p>
-               <p>Thank you for your message. We have received the following information:</p>
+               <p>Thank you for reaching out to us. We have received the following information:</p>
                <ul>
                  <li><b>Name:</b> ${bodyData.name}</li>
                  <li><b>Email:</b> ${bodyData.email}</li>
+                 <li><b>Subject:</b> ${bodyData.subject}</li>
+                 <li><b>Phone:</b> ${bodyData.phone}</li>
                  <li><b>Message:</b> ${bodyData.message}</li>
                </ul>
-               <p>We will get back to you soon.</p>`,
+               <p>We will get back to you shortly.</p>
+               <p>Best Regards,<br>My-TechLancer</p>`,
     };
 
     const adminMailOptions = {
         from: process.env.EMAIL,
         to: process.env.ADMIN_EMAIL, 
         subject: 'New Contact Form Submission',
-        text: `You have a new contact form submission from ${bodyData.name}.\n\nMessage: ${bodyData.message}\n\nClient Email: ${bodyData.email}`,
-        html: `<p>You have a new contact form submission from <b>${bodyData.name}</b>.</p>
-               <p>Message: ${bodyData.message}</p>
-               <p>Client Email: ${bodyData.email}</p>`,
-    };
+        text: `A new contact form submission has been received from ${bodyData.name}.\n\nDetails:\nName: ${bodyData.name}\nEmail: ${bodyData.email}\nSubject: ${bodyData.subject}\nPhone: ${bodyData.phone}\nMessage: ${bodyData.message}\n\nPlease follow up with the client as soon as possible.`,
+        html: `<p>A new contact form submission has been received from <b>${bodyData.name}</b>.</p>
+               <p><b>Details:</b></p>
+               <ul>
+                 <li><b>Name:</b> ${bodyData.name}</li>
+                 <li><b>Email:</b> ${bodyData.email}</li>
+                 <li><b>Subject:</b> ${bodyData.subject}</li>
+                 <li><b>Phone:</b> ${bodyData.phone}</li>
+                 <li><b>Message:</b> ${bodyData.message}</li>
+               </ul>`    };
 
     try {
-        // Send both emails
         const clientInfo = await transporter.sendMail(clientMailOptions);
         const adminInfo = await transporter.sendMail(adminMailOptions);
 
         return {
             clientMessageId: clientInfo.messageId,
             adminMessageId: adminInfo.messageId,
-            previewUrl: nodemailer.getTestMessageUrl(adminInfo), 
+            previewUrl: nodemailer.getTestMessageUrl(adminInfo),
         };
     } catch (error) {
         console.error('Error sending emails:', error);
+        throw new Error('Failed to send emails');
     }
 };
 
